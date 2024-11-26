3cdc6e17c87c5e7f7f722cebdc91986349b78677
M-Said-C <chaiebsaid.01@gmail.com>
2024-11-26 21:42:30 +0100

    init project

---
+++ src/main/java/com/project/gestion_examens/GestionExamensApplication.java
@@ -0,0 +1,13 @@
+package com.project.gestion_examens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionExamensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionExamensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/gestion_examens/GestionExamensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.gestion_examens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionExamensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
