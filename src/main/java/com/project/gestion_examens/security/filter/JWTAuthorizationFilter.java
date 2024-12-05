d7f1b34fc23781a5cf440b9ea14c0c7a9bf6c880
Yassine Slaoui <yassine_slaoui51@outlook.fr>
2024-12-05 20:55:53 +0100

    Revert "Enforces authorization using user roles from JWT."
    This reverts commit 775fa3189311f35d6d8edb15463e7ca570e3d691.

--- src/main/java/com/project/gestion_examens/security/filter/JWTAuthorizationFilter.java
+++ src/main/java/com/project/gestion_examens/security/filter/JWTAuthorizationFilter.java
@@ -12,22 +12,23 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 public class JWTAuthorizationFilter extends OncePerRequestFilter {
 
     private final JWTHelper jwtHelper;
-    private final UserDetailsService userDetailsService;
 
-    public JWTAuthorizationFilter(JWTHelper jwtHelper, UserDetailsService userDetailsService) {
+
+    public JWTAuthorizationFilter(JWTHelper jwtHelper) {
         this.jwtHelper = jwtHelper;
-        this.userDetailsService = userDetailsService;
     }
 
     @Override
@@ -41,10 +42,10 @@ public class JWTAuthorizationFilter extends OncePerRequestFilter {
             JWTVerifier jwtVerifier = JWT.require(algorithm).build();
             DecodedJWT decodedJWT = jwtVerifier.verify(accessToken);
             String email = decodedJWT.getSubject();
-            UserDetails userDetails = userDetailsService.loadUserByUsername(email);
-            UsernamePasswordAuthenticationToken authenticationToken =
-                    new UsernamePasswordAuthenticationToken(userDetails.getUsername(),
-                    null, userDetails.getAuthorities());
+            Collection<GrantedAuthority> authorities = new ArrayList<>();
+            decodedJWT.getClaim("roles").asList(String.class).forEach(role -> authorities.add(new SimpleGrantedAuthority(role)));
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(email,
+                    null, authorities);
             SecurityContextHolder.getContext().setAuthentication(authenticationToken);
         }
         filterChain.doFilter(request, response);
