59a4c337adbd53be5d05932f609b61818266679f
aidocons <consultant-butlyai@aidodev.com>
2024-12-20 00:13:52 +0100

    resolve conflicts

--- src/main/java/com/project/gestion_examens/controllers/EnseignantController.java
+++ src/main/java/com/project/gestion_examens/controllers/EnseignantController.java
@@ -1,9 +1,6 @@
 package com.project.gestion_examens.controllers;
 
-<<<<<<< HEAD
 import com.project.gestion_examens.dto.request.AddEnseignantDTO;
-=======
->>>>>>> 04a9a2ff7180845565d5734b4e64bbd0e402c780
 import com.project.gestion_examens.dto.response.EnseignantResponseDTO;
 import com.project.gestion_examens.entities.Enseignant;
 import com.project.gestion_examens.services.impl.EnseignantService;
